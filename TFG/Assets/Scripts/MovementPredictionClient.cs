5ddb1f629daae5e15ca84866792c8db4f54b8d65
constan <constantinocalladoperez@gmail.com>
2015-06-01 16:27:42 +0200

    Prediccion basica mejorada, todavia hay que controlar colisiones con muros

--- TFG/Assets/Scripts/MovementPredictionClient.cs
+++ TFG/Assets/Scripts/MovementPredictionClient.cs
@@ -3,44 +3,29 @@ using System.Collections;
 
 public class MovementPredictionClient : BasicMovementClient 
 {	
-	private Vector3 posAnterior;
-	private Vector3 velPredicted;
-	private Vector3 posNueva;
+	private Vector2 posAseguradaAnterior;
+	private Vector2 posPredicted;
+	private Vector2 posAseguradaNueva;
 	private Player refJugador;
-	private Vector3 posPredicted;
 	private bool initialized = false;
-
+	private Transform refTransform;
 	public void Awake()
 	{
 		base.Awake();
-
+		refTransform = transform;
 		refJugador = gameObject.GetComponent<Player>();
 	}
 
 	// Update is called once per frame
 	void Update () 
 	{
-		// Si la diferencia entre posiciones es pequeña procedemos a mover el jugador segun la prediccion
-		if((base.transformRef.position - posNueva).sqrMagnitude < 2 && initialized)
+		if(((Vector2)refTransform.position - posAseguradaNueva).sqrMagnitude < 2)
 		{
-			posPredicted = base.transformRef.position + velPredicted;
-
-			if(Scenario.scenarioRef.arrayNivel[(int)(posPredicted.y + 0.5f), (int)(posPredicted.x + 0.5f)] != 0)
-			//if(true)
-			{
-				base.transformRef.position = Vector2.MoveTowards(base.transformRef.position, 
-				                                                 base.transformRef.position + velPredicted, Time.deltaTime * refJugador.speed);
-			}
-			else
-			{
-				Debug.Log("COLISION ENFRENTE");
-			}
+			refTransform.position = Vector2.MoveTowards(refTransform.position, posPredicted, Time.deltaTime * refJugador.speed);
 		}
-		// Sino lo movemos directamente sin prediccion
 		else
 		{
-			Debug.Log("Posicion demasiado lejana, movemos el jugador sin prediccion");
-			base.transformRef.position = posNueva;
+			refTransform.position = posAseguradaNueva;
 		}
 	}
 
@@ -48,16 +33,15 @@ public class MovementPredictionClient : BasicMovementClient
 	{
 		if(initialized)
 		{
-			posAnterior = posNueva;
-			posNueva = positionRecieved;
-			base.transformRef.position = positionRecieved;
-			velPredicted = (posNueva - posAnterior).normalized;
+			posAseguradaAnterior = posAseguradaNueva;
+			posAseguradaNueva = positionRecieved;
+			posPredicted = posAseguradaNueva + (posAseguradaNueva - posAseguradaAnterior);
 		}
 		else
 		{
-			posAnterior = posNueva = positionRecieved;
+			// Al principio ponemos todas las posiciones a la recibida
+			posAseguradaAnterior = posAseguradaNueva = posPredicted = positionRecieved;
+			initialized = true;
 		}
-
-		initialized = true;
 	}
 }

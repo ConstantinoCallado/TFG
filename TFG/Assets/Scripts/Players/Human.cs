df9306878107817232614f53fcfa54c9ca7a2312
constan <constantinocalladoperez@gmail.com>
2015-08-20 10:34:16 +0200

    Las bolitas a recoger se han cambiado por tuercas

--- TFG/Assets/Scripts/Players/Human.cs
+++ TFG/Assets/Scripts/Players/Human.cs
@@ -5,8 +5,8 @@ public class Human : Player
 {
 	public bool aggressiveMode = false;
 	float aggressiveTimeEnd = 0;
-	//const float aggressiveTime = 8;
-	const float aggressiveTime = 20;
+	const float aggressiveTime = 8;
+	//const float aggressiveTime = 20;
 	public SightableHuman sightable;
 	
 	public static Human humanRef;

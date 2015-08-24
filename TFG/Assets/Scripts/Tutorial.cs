1ef426049dc60e332f59bcd887d174d97dd6a6d0
constan <constantinocalladoperez@gmail.com>
2015-08-24 20:04:48 +0200

    Tutorial terminado

---
+++ TFG/Assets/Scripts/Tutorial.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Tutorial : MonoBehaviour 
+{
+	public GameObject botonSiguiente;
+	public GameObject botonAnterior;
+
+	public List<GameObject> listaPanelesTutorial = new List<GameObject>();
+	private short panelSeleccionado = 0;
+
+	void OnEnable()
+	{
+		OcultarTodos();
+		panelSeleccionado = 0;
+
+		CargarPanel(panelSeleccionado);
+	}
+
+	void OcultarTodos()
+	{
+		for(int i=0; i<listaPanelesTutorial.Count; i++)
+		{
+			listaPanelesTutorial[i].SetActive(false);
+		}
+	}
+
+	public void BotonSiguiente()
+	{
+		if(panelSeleccionado < listaPanelesTutorial.Count-1)
+		{
+			listaPanelesTutorial[panelSeleccionado].SetActive(false);
+			++panelSeleccionado;
+			CargarPanel(panelSeleccionado);
+		}
+	}
+
+	public void BotonAnterior()
+	{
+		if(panelSeleccionado > 0)
+		{
+			listaPanelesTutorial[panelSeleccionado].SetActive(false);
+			--panelSeleccionado;
+			CargarPanel(panelSeleccionado);
+		}
+	}
+
+	void CargarPanel(int numero)
+	{
+		listaPanelesTutorial[numero].SetActive(true);
+
+		if(numero == listaPanelesTutorial.Count-1)
+		{
+			botonSiguiente.SetActive(false);
+		}
+		else
+		{
+			botonSiguiente.SetActive(true);
+		}
+
+		if(numero == 0)
+		{
+			botonAnterior.SetActive(false);
+		}
+		else
+		{
+			botonAnterior.SetActive(true);
+		}
+	}
+}
